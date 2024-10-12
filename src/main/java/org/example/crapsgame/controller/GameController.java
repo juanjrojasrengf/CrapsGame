c2de55bb7f1d5ba3847386567b002e409d8d8abc
reinel_vargas <reinel.vargas@correounivalle.edu.co>
2024-10-12 13:16:20 -0500

    Added the logic to set the game winner

--- src/main/java/org/example/crapsgame/controller/GameController.java
+++ src/main/java/org/example/crapsgame/controller/GameController.java
@@ -18,6 +18,7 @@ public class GameController {
     private ImageView dice1ImageView, dice2ImageView;
 
     Dice dice1, dice2;
+    Game game = new Game();
     ArrayList<Game> games = new ArrayList<Game>();
 
     public GameController() {
@@ -27,10 +28,40 @@ public class GameController {
 
     @FXML
     public void onHandleButtonRollTheDice(ActionEvent event) {
+        this.game.setShootCount();
         this.dice1.rollDice();
         this.dice2.rollDice();
         this.dice1ImageView.setImage(this.dice1.getDiceImage());
         this.dice2ImageView.setImage(this.dice2.getDiceImage());
-    }
 
+        int totalValue = this.dice1.getValue() + this.dice2.getValue();
+
+        if (this.game.getShootCount() == 1 ){
+            if (totalValue == 7 || totalValue == 11){
+                this.game.setWinner();
+                System.out.println("GANASTE!");
+            }
+            else if (totalValue == 2 || totalValue == 3 || totalValue == 12){
+                this.game.setLoser();
+                System.out.println("PERDISTE!");
+            }
+            else{
+                this.game.setPoint(totalValue);
+                System.out.println("Tu punto es: " + totalValue);
+            }
+        }
+        else{
+            if (totalValue != 7){
+                if (this.game.getPoint() == totalValue){
+                    this.game.setWinner();
+                    System.out.println("GANASTE");
+                }
+            }
+            else{
+                this.game.setLoser();
+                System.out.println("PERDISTE");
+            }
+        }
+
+    }
 }
